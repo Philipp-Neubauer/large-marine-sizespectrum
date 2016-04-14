c4672d647044027f2c4909ae7086068734007133
Philipp Neubauer <neubauer.phil@gmail.com>
2016-04-14 14:00:38 +1200

    working docker

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+eval $(docker-machine env default)
 set -ex
 
 docker build -t dragonfly/sizespectra .
 
-docker run --rm -it -v $PWD/..:/work -w /work/ \
+docker run --rm -i -v $PWD/..:/work -w /work/large-marine-sizespectrum \
   dragonfly/sizespectra ./build.sh
+  
