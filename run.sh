7cd0f9d5632bc774f2aa9bb4be53a093ddfb5845
Philipp Neubauer <neubauer.phil@gmail.com>
2016-05-17 14:35:32 +1200

    some tidying

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 eval $(docker-machine env default)
 set -ex
 
-docker pull mmmmk/size-spectra-docker
+#docker pull mmmmk/size-spectra-docker
 
 docker run --rm -i -v $PWD/..:/work -w /work/large-marine-sizespectrum \
   mmmmk/size-spectra-docker ./build.sh
